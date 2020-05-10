0dfd0a375fbe6c25b8ec60a78721d0cca892fac9
BangHyeonJun <bhj2849@naver.com>
2020-05-10 22:06:28 +0900

    [CLN] 페이지 전환 참고 자료 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import { AppProps } from 'next/app';
 // 글로벌 스타일
 import './styles/global.scss';
 
+// 페이지 전환 참고
+// https://www.youtube.com/watch?v=zIDpZi-36Qs
 function App({ Component, pageProps }: AppProps) {
     return (
         <AnimatePresence exitBeforeEnter>
