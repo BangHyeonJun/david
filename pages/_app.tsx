8cffe6d5b449d9c4ce5f0e0594a517e443e6014f
BangHyeonJun <bhj2849@naver.com>
2020-05-10 20:03:59 +0900

    [ENH] 글로벌 스타일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { AppProps } from 'next/app';
 
+// 글로벌 스타일
+import './styles/global.scss';
+
 function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import Head from 'next/head';
 
 // 스타일
-import classNames from 'classnames/bind';
-import styled from './index.module.scss';
-const cx = classNames.bind(styled);
 
 // 컴포넌트
 
 export default function Home() {
-    return <div className={cx('test')}>테스트</div>;
+    return <div>테스트</div>;
 }
