602f17a0eb962a67e6a383a88b67e0cbbdcae9b6
david <david@acriil.com>
2020-05-11 19:49:18 +0900

    [ENH] 전체 레이아웃 작업 진행중

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import classNames from 'classnames/bind';
+
+import styles from './Layout.module.scss';
+const cx = classNames.bind(styles);
 
 const Layout = props => (
-    <div>
+    <div className={cx('wrap')}>
         <Header />
         {props.children}
     </div>
