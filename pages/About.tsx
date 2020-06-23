61c9e9700214a2bf54c14ca83c5530c9be39b81a
BangHyeonJun <bhj2849@naver.com>
2020-06-23 19:14:12 +0900

    About 이름 추가

--- pages/About.tsx
+++ pages/About.tsx
@@ -2,59 +2,26 @@ import React from 'react';
 
 // 스타일
 import classNames from 'classnames/bind';
-// import styles from './about.module.scss';
-// const cx = classNames.bind(styles);
+import styles from './About.module.scss';
+const cx = classNames.bind(styles);
 
 import Layout from './components/Layout/Layout';
 
 import { motion, AnimatePresence } from 'framer-motion';
 
-function about() {
-    const pageVariants = {
-        initial: {
-            // opacity: 0,
-            transform: `rotateY(360deg)`,
-            perspectiveOrigin: 'center',
-            perspective: '300px',
-            transition: { duration: 0.5 },
-        },
-        in: {
-            // opacity: 1,
-            transform: `rotateY(0deg)`,
-            perspectiveOrigin: 'center',
-            perspective: '0px',
-            transition: { duration: 2 },
-        },
-        out: {
-            // opacity: 0,
-            transform: `rotateY(360deg)`,
-            perspectiveOrigin: 'center',
-            perspective: '300px',
-            transition: { duration: 2 },
-            // transition: { duration: 2 },
-        },
-    };
-
+function About() {
     return (
         <Layout title={'ABOUT'}>
-            <div>테스트</div>
-            {/* <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                className={cx('wrap')}
-            > */}
-            {/* <motion.div
-                animate={{ x: 0, opacity: 1 }}
-                initial={{ x: 60, opacity: 0 }}
-                className={cx('cardWrap')}
-            >
-                <h1>내 소개</h1>
-            </motion.div> */}
-            {/* </motion.div> */}
+            <div className={cx('world')}>
+                <div className={cx('')}>
+                    <div className={cx('name')}>
+                        <span>Bang Hyeon Jun | David Bang</span>
+                        <span>Welcome, this page is my portfolio page</span>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }
 
-export default about;
+export default About;
